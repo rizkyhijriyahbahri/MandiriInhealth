6610bc0e773042f72f5470e2570492fe24a8d74b
Muhammad Rizky Hijriyah Bahri <v-rizky.hijriyah@anteraja.id>
2022-12-22 14:29:26 +0700

    Add project files.

---
+++ Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MandiriInhealth.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MandiriInhealth.WebApiApplication" Language="C#" %>
